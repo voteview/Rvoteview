0f6d9e9e2ecea27ead29fd4d2321315f9f93df7f
Luke Sonnet <luke.sonnet@gmail.com>
2016-05-20 15:42:26 -0700

    Vignette and tests pass

--- tests/testthat/test_search.R
+++ tests/testthat/test_search.R
@@ -10,7 +10,7 @@ test_that('search function options query leela correctly', {
   expect_that(voteview_search('Iraq', startdate = '2013-10-01'), not(throws_error()))
   expect_that(voteview_search('Iraq', enddate = '2014-10-01'), not(throws_error()))
   expect_that(voteview_search('Iraq', chamber = "House"), not(throws_error()))
-  expect_that(voteview_search('Iraq', chamber = "House", session = 110:113), not(throws_error()))
+  expect_that(voteview_search('Iraq', chamber = "House", congress = 110:113), not(throws_error()))
 })
 
 test_that('search function returns example dataframe with correct number of votes', {
@@ -19,8 +19,8 @@ test_that('search function returns example dataframe with correct number of vote
   
   expect_is(df, 'data.frame')
   expect_is(df_sub, 'data.frame')
-  expect_equal(nrow(df), 142)
-  expect_equal(nrow(df_sub), 136)
+  expect_equal(nrow(df), 32)
+  expect_equal(nrow(df_sub), 31)
   expect_is(df_sub$id, "character")
   expect_identical(length(unique(df_sub$id)), nrow(df_sub))
 })
