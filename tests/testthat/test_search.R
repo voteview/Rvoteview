99a959dccf1105fd01134605e85f45a6f5fb1a33
Luke Sonnet <luke.sonnet@gmail.com>
2016-09-28 07:26:53 -0700

    Added some new tests

--- tests/testthat/test_search.R
+++ tests/testthat/test_search.R
@@ -5,6 +5,16 @@ test_that('search function hits database', {
   expect_error(voteview_search('test'), NA)
 })
 
+test_that('query returned correctly', {
+  expect_equal(attr(voteview_search('rhodesia', enddate = '2000'),
+                    'qstring'),
+               '(rhodesia) AND (enddate:2000)')
+  
+  expect_equal(attr(voteview_search('rhodesia AND (enddate:2000 OR congress:110)'),
+                    'qstring'),
+               'rhodesia AND (enddate:2000 OR congress:110)')
+})
+
 test_that('search function options query leela correctly', {
   expect_error(voteview_search('Iraq', startdate = 2014), NA)
   expect_error(voteview_search('Iraq', startdate = '2013-10-01'), NA)
@@ -14,13 +24,18 @@ test_that('search function options query leela correctly', {
 })
 
 test_that('search function returns example dataframe with correct number of votes', {
-  df <- voteview_search('rhodesia')
-  df_sub <- voteview_search('rhodesia', startdate = 1970)
+  df <- voteview_search('rhodesia', enddate = 1975)
+  df_sub <- voteview_search('rhodesia', enddate = 1980)
   
   expect_is(df, 'data.frame')
   expect_is(df_sub, 'data.frame')
-  expect_equal(nrow(df), 32)
-  expect_equal(nrow(df_sub), 31)
+  expect_equal(nrow(df), 8)
+  expect_equal(nrow(df_sub), 32)
   expect_is(df_sub$id, "character")
   expect_identical(length(unique(df_sub$id)), nrow(df_sub))
+  
+})
+
+test_that('unicode searches work', {
+  voteview_search('\U00B6')
 })
