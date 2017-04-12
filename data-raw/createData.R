edaf94428b7d2b0607336e129a20624e400c5af5
Luke Sonnet <luke.sonnet@gmail.com>
2017-04-12 11:36:22 -0700

    Updates

--- data-raw/createData.R
+++ data-raw/createData.R
@@ -9,4 +9,9 @@ save(lpOpinion, file = "../data/lpOpinion.rda")
 states <- read.csv("statecodes.csv", header = F, stringsAsFactors = F)
 names(states) <- c("state_code", "state_abbrev", "state_name")
 
+state_table <- read.csv("state_table.csv", stringsAsFactors = F)
+state_table$state_name <- toupper(state_table$name)
+states <- merge(states, state_table[, c("state_name", "census_region_name", "census_division_name")],
+                all.x = T, all.y = F)
+states
 save(states, file = "../data/states.rda")
